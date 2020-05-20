c587cd3cd82b4616cd341039f4939d1f49d40086
Adam Wight <adam.wight@wikimedia.de>
2020-05-20 17:23:17 +0200

    Revert "Move OOUI declaration out of function"
    Fails in production for some reason!
    
    This reverts commit 778a699df1b40b117888db3a820f7c2a7d8fa8b7.

--- EmbeddedSurvey.js
+++ EmbeddedSurvey.js
@@ -70,8 +70,6 @@
 	}
 
 	function getSurveyStorageKey( name ) {
-		// FIXME: redundant, but we want to be absolutely sure to namespace in case
-		//  it's left off of the survey.name
 		return 'wmde-tw-survey-' + name.replace( / /g, '-' );
 	}
 
@@ -84,228 +82,227 @@
 		mw.storage.set( storageId, token );
 	}
 
-	/**
-	 * @class EmbeddedSurvey
-	 * @extends OO.ui.StackLayout
-	 *
-	 * @constructor
-	 * @param {Object} config
-	 */
-	function EmbeddedSurvey( config ) {
-		this.initialize( config );
-	}
-
-	extend( EmbeddedSurvey.prototype, {
+	function show() {
 		/**
-		 * Specifies partials (sub-templates) for use by the widget
-		 * @property {Object}
-		 */
-		templatePartials: {
-			// eslint-disable-next-line no-jquery/no-parse-html-literal
-			initialPanel: $(
-				'<div>' +
-				'<strong data-question></strong>' +
-				'<p data-description></p>' +
-				'<div class="survey-button-container"></div>' +
-				'<div class="survey-footer" data-footer></div>' +
-				'</div>'
-			),
-			// eslint-disable-next-line no-jquery/no-parse-html-literal
-			finalPanel: $(
-				'<div>' +
-				'<strong data-finalHeading></strong>' +
-				'<div class="survey-footer" data-footer></div>' +
-				'</div>'
-			)
-		},
-		/**
-		 * A set of default options that are merged with config passed into the initialize function.
-		 * This is likely to change so currently no options are documented.
-		 * @cfg {Object} defaults Default options hash.
-		 */
-		defaults: {
-			templateData: {
-				finalHeading: mw.msg( 'wmde-tw-survey-thank-you-notice' ),
-				footer: mw.message( 'ext-quicksurveys-survey-privacy-policy-default-text' ).parse()
-			},
-			PanelLayout: {
-				expanded: false,
-				framed: false,
-				padded: true,
-				classes: [ 'message content' ]
-			},
-			scrollable: false,
-			expanded: false,
-			classes: [ 'panel panel-inline visible ext-quick-survey-panel' ]
-		},
-		/**
-		 * Initialise a widget.
+		 * @class EmbeddedSurvey
+		 * @extends OO.ui.StackLayout
 		 *
+		 * @constructor
 		 * @param {Object} config
 		 */
-		initialize: function ( config ) {
-			var event;
-
-			this.config = config || {};
-			$.extend( true, this.config, this.defaults );
-
-			if ( config.survey.privacyPolicy ) {
-				// eslint-disable-next-line mediawiki/msg-doc
-				this.config.templateData.footer = mw.message( config.survey.privacyPolicy ).parse();
-			}
-
-			// setup initial panel
-			this.initialPanel = this.widget( 'PanelLayout', 'initialPanel' );
-
-			// setup final panel
-			this.finalPanel = this.widget( 'PanelLayout', 'finalPanel' );
-
-			// Set the buttons
-			this.renderButtons();
-
-			// setup stack
-			EmbeddedSurvey.super.call( this, $.extend( {}, config, {
-				items: [ this.initialPanel, this.finalPanel ]
-			} ) );
-
-			event = {
-				beaconCapable: !!navigator.sendBeacon,
-				surveySessionToken: this.config.surveySessionToken,
-				pageviewToken: this.config.pageviewToken,
-				surveyCodeName: this.config.survey.name,
-				eventName: 'eligible'
-			};
-
-			if ( window.performance && performance.now ) {
-				event.performanceNow = Math.round( performance.now() );
-			}
-
-			mw.eventLog.logEvent( 'QuickSurveyInitiation', event );
-		},
-		/**
-		 * Render and append buttons (and a freeform input if set) to
-		 * the initial panel
-		 */
-		renderButtons: function () {
-			var $btnContainer = this.initialPanel.$element.find( '.survey-button-container' ),
-				answers = this.config.survey.answers,
-				answerCheckboxes,
-				answerOptions,
-				submitButton;
-
-			answerOptions = answers.map( function ( answer ) {
-				return {
-					data: answer,
-					label: mw.msg( answer )
-				};
-			} );
-			answerCheckboxes = new OO.ui.CheckboxMultiselectInputWidget( {
-				// eslint-disable-next-line mediawiki/msg-doc
-				options: answerOptions
-			} );
-
-			answerCheckboxes.$element.appendTo( $btnContainer );
-
-			submitButton = new OO.ui.ButtonWidget( {
-				label: mw.msg( 'ext-quicksurveys-internal-freeform-survey-submit-button' ),
-				flags: 'progressive'
-			} );
-			submitButton.$element.appendTo( $btnContainer );
-
-			submitButton.connect( this, {
-				click: [ 'onClickSubmitButton', answerCheckboxes ]
-			} );
-		},
-		/**
-		 * Make a brand spanking new OOUI widget from a template partial
-		 *
-		 * @param {string} widgetName a valid OOUI widget
-		 * @param {string} [templatePartialName] name of a registered template partial
-		 * @param {Object} [options] further options to be passed to the widget
-		 * @return {*} OOUI widget instance
-		 */
-		widget: function ( widgetName, templatePartialName, options ) {
-			var templateClone,
-				template = this.templatePartials[ templatePartialName ],
-				config = $.extend( {}, this.config[ widgetName ], options ),
-				templateData = this.config.templateData;
-
-			if ( template ) {
-				templateClone = template.clone();
-				templateClone.find( '[data-question]' ).text( templateData.question );
-				templateClone.find( '[data-description]' ).text( templateData.description );
-				templateClone.find( '[data-footer]' ).html( templateData.footer );
-				templateClone.find( '[data-finalHeading]' ).text( templateData.finalHeading );
-				config.$content = templateClone;
-			}
-
-			return new OO.ui[ widgetName ]( config );
-		},
-		/**
-		 * Log the answer to Schema:QuickSurveysResponses
-		 * See {@link https://meta.wikimedia.org/wiki/Schema:QuickSurveysResponses}
-		 *
-		 * @param {string} answer
-		 * @return {jQuery.Deferred}
-		 */
-		log: function ( answer ) {
-			var survey = this.config.survey,
-				skin = mw.config.get( 'skin' ),
-				// FIXME: remove this when SkinMinervaBeta is renamed to 'minerva-beta'.
-				mobileMode = mw.config.get( 'wgMFMode' );
-
-			// On mobile differentiate between minerva stable and beta
-			// by appending 'beta' to 'minerva'
-			if ( skin === 'minerva' && mobileMode === 'beta' ) {
-				skin += mobileMode;
-			}
-
-			return mw.eventLog.logEvent( 'QuickSurveysResponses', {
-				namespaceId: mw.config.get( 'wgNamespaceNumber' ),
-				surveySessionToken: this.config.surveySessionToken,
-				pageviewToken: this.config.pageviewToken,
-				pageId: mw.config.get( 'wgArticleId' ),
-				pageTitle: mw.config.get( 'wgPageName' ),
-				surveyCodeName: survey.name,
-				surveyResponseValue: answer,
-				platform: 'web',
-				skin: skin,
-				isTablet: !this.config.isMobileLayout,
-				userLanguage: mw.config.get( 'wgContentLanguage' ),
-				isLoggedIn: !mw.user.isAnon(),
-				editCountBucket: getEditCountBucket( mw.config.get( 'wgUserEditCount' ) ),
-				countryCode: 'Unknown'
-			} );
-		},
-
-		/**
-		 * @param {OO.ui.CheckboxMultiselectInputWidget} checkboxes
-		 * @private
-		 */
-		onClickSubmitButton: function ( checkboxes ) {
-			var selections = checkboxes.getValue();
-
-			this.submit( selections.join(',') );
-		},
-
-		/**
-		 * Submit user's answer to the backend and show the next panel
-		 *
-		 * @param {string} answer
-		 */
-		submit: function ( answer ) {
-			this.log( answer );
-			/**
-			 * @event dismiss fired when any of the buttons in the survey are selected.
-			 */
-			this.emit( 'dismiss' );
-			this.setItem( this.finalPanel );
+		function EmbeddedSurvey( config ) {
+			this.initialize( config );
 		}
-	} );
 
-	function show() {
-		// FIXME: Move back into class if it can be extracted into an external module protected from dependency loading.
 		OO.inheritClass( EmbeddedSurvey, OO.ui.StackLayout );
+
+		extend( EmbeddedSurvey.prototype, {
+			/**
+			 * Specifies partials (sub-templates) for use by the widget
+			 * @property {Object}
+			 */
+			templatePartials: {
+				// eslint-disable-next-line no-jquery/no-parse-html-literal
+				initialPanel: $(
+					'<div>' +
+					'<strong data-question></strong>' +
+					'<p data-description></p>' +
+					'<div class="survey-button-container"></div>' +
+					'<div class="survey-footer" data-footer></div>' +
+					'</div>'
+				),
+				// eslint-disable-next-line no-jquery/no-parse-html-literal
+				finalPanel: $(
+					'<div>' +
+					'<strong data-finalHeading></strong>' +
+					'<div class="survey-footer" data-footer></div>' +
+					'</div>'
+				)
+			},
+			/**
+			 * A set of default options that are merged with config passed into the initialize function.
+			 * This is likely to change so currently no options are documented.
+			 * @cfg {Object} defaults Default options hash.
+			 */
+			defaults: {
+				templateData: {
+					finalHeading: mw.msg( 'wmde-tw-survey-thank-you-notice' ),
+					footer: mw.message( 'ext-quicksurveys-survey-privacy-policy-default-text' ).parse()
+				},
+				PanelLayout: {
+					expanded: false,
+					framed: false,
+					padded: true,
+					classes: [ 'message content' ]
+				},
+				scrollable: false,
+				expanded: false,
+				classes: [ 'panel panel-inline visible ext-quick-survey-panel' ]
+			},
+			/**
+			 * Initialise a widget.
+			 *
+			 * @param {Object} config
+			 */
+			initialize: function ( config ) {
+				var event;
+
+				this.config = config || {};
+				$.extend( true, this.config, this.defaults );
+
+				if ( config.survey.privacyPolicy ) {
+					// eslint-disable-next-line mediawiki/msg-doc
+					this.config.templateData.footer = mw.message( config.survey.privacyPolicy ).parse();
+				}
+
+				// setup initial panel
+				this.initialPanel = this.widget( 'PanelLayout', 'initialPanel' );
+
+				// setup final panel
+				this.finalPanel = this.widget( 'PanelLayout', 'finalPanel' );
+
+				// Set the buttons
+				this.renderButtons();
+
+				// setup stack
+				EmbeddedSurvey.super.call( this, $.extend( {}, config, {
+					items: [ this.initialPanel, this.finalPanel ]
+				} ) );
+
+				event = {
+					beaconCapable: !!navigator.sendBeacon,
+					surveySessionToken: this.config.surveySessionToken,
+					pageviewToken: this.config.pageviewToken,
+					surveyCodeName: this.config.survey.name,
+					eventName: 'eligible'
+				};
+
+				if ( window.performance && performance.now ) {
+					event.performanceNow = Math.round( performance.now() );
+				}
+
+				mw.eventLog.logEvent( 'QuickSurveyInitiation', event );
+			},
+			/**
+			 * Render and append buttons (and a freeform input if set) to
+			 * the initial panel
+			 */
+			renderButtons: function () {
+				var $btnContainer = this.initialPanel.$element.find( '.survey-button-container' ),
+					answers = this.config.survey.answers,
+					answerCheckboxes,
+					answerOptions,
+					submitButton;
+
+				answerOptions = answers.map( function ( answer ) {
+					return {
+						data: answer,
+						label: mw.msg( answer )
+					};
+				} );
+				answerCheckboxes = new OO.ui.CheckboxMultiselectInputWidget( {
+					// eslint-disable-next-line mediawiki/msg-doc
+					options: answerOptions
+				} );
+
+				answerCheckboxes.$element.appendTo( $btnContainer );
+
+				submitButton = new OO.ui.ButtonWidget( {
+					label: mw.msg( 'ext-quicksurveys-internal-freeform-survey-submit-button' ),
+					flags: 'progressive'
+				} );
+				submitButton.$element.appendTo( $btnContainer );
+
+				submitButton.connect( this, {
+					click: [ 'onClickSubmitButton', answerCheckboxes ]
+				} );
+			},
+			/**
+			 * Make a brand spanking new OOUI widget from a template partial
+			 *
+			 * @param {string} widgetName a valid OOUI widget
+			 * @param {string} [templatePartialName] name of a registered template partial
+			 * @param {Object} [options] further options to be passed to the widget
+			 * @return {*} OOUI widget instance
+			 */
+			widget: function ( widgetName, templatePartialName, options ) {
+				var templateClone,
+					template = this.templatePartials[ templatePartialName ],
+					config = $.extend( {}, this.config[ widgetName ], options ),
+					templateData = this.config.templateData;
+
+				if ( template ) {
+					templateClone = template.clone();
+					templateClone.find( '[data-question]' ).text( templateData.question );
+					templateClone.find( '[data-description]' ).text( templateData.description );
+					templateClone.find( '[data-footer]' ).html( templateData.footer );
+					templateClone.find( '[data-finalHeading]' ).text( templateData.finalHeading );
+					config.$content = templateClone;
+				}
+
+				return new OO.ui[ widgetName ]( config );
+			},
+			/**
+			 * Log the answer to Schema:QuickSurveysResponses
+			 * See {@link https://meta.wikimedia.org/wiki/Schema:QuickSurveysResponses}
+			 *
+			 * @param {string} answer
+			 * @return {jQuery.Deferred}
+			 */
+			log: function ( answer ) {
+				var survey = this.config.survey,
+					skin = mw.config.get( 'skin' ),
+					// FIXME: remove this when SkinMinervaBeta is renamed to 'minerva-beta'.
+					mobileMode = mw.config.get( 'wgMFMode' );
+
+				// On mobile differentiate between minerva stable and beta
+				// by appending 'beta' to 'minerva'
+				if ( skin === 'minerva' && mobileMode === 'beta' ) {
+					skin += mobileMode;
+				}
+
+				return mw.eventLog.logEvent( 'QuickSurveysResponses', {
+					namespaceId: mw.config.get( 'wgNamespaceNumber' ),
+					surveySessionToken: this.config.surveySessionToken,
+					pageviewToken: this.config.pageviewToken,
+					pageId: mw.config.get( 'wgArticleId' ),
+					pageTitle: mw.config.get( 'wgPageName' ),
+					surveyCodeName: survey.name,
+					surveyResponseValue: answer,
+					platform: 'web',
+					skin: skin,
+					isTablet: !this.config.isMobileLayout,
+					userLanguage: mw.config.get( 'wgContentLanguage' ),
+					isLoggedIn: !mw.user.isAnon(),
+					editCountBucket: getEditCountBucket( mw.config.get( 'wgUserEditCount' ) ),
+					countryCode: 'Unknown'
+				} );
+			},
+
+			/**
+			 * @param {OO.ui.CheckboxMultiselectInputWidget} checkboxes
+			 * @private
+			 */
+			onClickSubmitButton: function ( checkboxes ) {
+				var selections = checkboxes.getValue();
+
+				this.submit( selections.join(',') );
+			},
+
+			/**
+			 * Submit user's answer to the backend and show the next panel
+			 *
+			 * @param {string} answer
+			 */
+			submit: function ( answer ) {
+				this.log( answer );
+				/**
+				 * @event dismiss fired when any of the buttons in the survey are selected.
+				 */
+				this.emit( 'dismiss' );
+				this.setItem( this.finalPanel );
+			}
+		} );
 
 		for ( var survey of surveyConfig.surveys ) {
 			var root = $( survey.injectInto ),
@@ -397,5 +394,6 @@
 		} else {
 			showNotice( isDisplayed );
 		}
+		// TODO: else, show placeholder and optional reason.
 	} );
 } )();
